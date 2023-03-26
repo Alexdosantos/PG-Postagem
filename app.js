4ef9ca2e7c2cbb61807b0a5b7a242708fd987ba1
Alex dos Santos <125704045+Alexdosantos@users.noreply.github.com>
2023-03-26 00:20:57 -0300

    Código atualizado
    correção do get

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-
 // AQUI É O CÓDIGO DE ABRIR E FECHAR O MODAL
-
-
 const button = document.querySelector("#btnModal")
 const Modal = document.querySelector("dialog")
 const closeModal = document.querySelector("#closeModal")
-
-
 
 button.onclick = function() {
     Modal.showModal()
@@ -14,79 +9,72 @@ button.onclick = function() {
 }
 
 closeModal.onclick = function(){
-
     Modal.close()
 }
-    
-
 
 // AQUI COMEÇA A INTEGRAÇÃO COM A API
-
-
-
-
 const getPosts = async () => {
-    const postagens =  document.getElementById('#content')
-    const  apiResponse = await fetch("http://localhost:3000/Cadastro-Usuarios")
-    const post = await apiResponse.json()
-    console.log(post)
-
-    
-    let conteudo = ''
-
-    
+  const postagens =  document.getElementById('content')
+  const  apiResponse = await fetch("http://localhost:3000/Cadastro-Usuarios")
+  const post = await apiResponse.json()
+  console.log(post)
+  let conteudo = ''
 
   post.forEach(noticia => {
     conteudo = conteudo + `
       <div>${noticia.titulo}</div>
-        <div>
-            <h4>${noticia.titulo}</h4>
-            <h4>Por ${noticia.autor}</h4>
-            <img class="imagem" src="${noticia.imagem}" />
-            <p>${noticia.texto}</p>
-        </div>
+      <div>
+        <h4>${noticia.titulo}</h4>
+        <h4>Por ${noticia.autor}</h4>
+        <img class="imagem" src="${noticia.imagem}" />
+        <p>${noticia.texto}</p>
+      </div>
     `
-})
+  })
 
-postagens.innerHTML = conteudo
-
+  postagens.innerHTML = conteudo
 }
 
+window.addEventListener('DOMContentLoaded', getPosts)
 
 
+
+
+//AQUI É A CRIAÇÃO DO POST 
 const createPost = async (post) => {
     await fetch("http://localhost:3000/Cadastro-Usuarios", {
       method: "POST",
       headers: {
-        'Accept': 'application/json, text/plain, */*',
+        'Accept': 'application/json, text/plain, /',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(post)
     });
   }
 
-  const addPost = async () => {
+const addPost = async () => {
 
-    const titulo = document.querySelector('#titulo').value
-    const autor = document.querySelector('#autor').value
-    const imagem= document.querySelector('#imagemDestaque').value
-    const texto = document.querySelector('#caixaMaior').value
+  const titulo = document.querySelector('#titulo').value
+  const autor = document.querySelector('#autor').value
+  const imagem= document.querySelector('#imagemDestaque').value
+  const texto = document.querySelector('#caixaMaior').value
 
+  const post = {
+    titulo,
+    autor,
+    imagem,
+    texto
+  }
 
-    const post = {
-        titulo,
-        autor,
-        imagem,
-        texto
-    }
-
-    await createPost(post)
-
-
+  await createPost(post)
 }
 
 document.querySelector('#submit').addEventListener('click', addPost)
 
+
+
+
+//AQUI É ONDE DELETA O POST
 
 const deletePost = async (id) => {
     await fetch(`http://localhost:3000/Cadastro-Usuarios/${id}`, {
@@ -94,22 +82,18 @@ const deletePost = async (id) => {
     });
   }
   
-  document.querySelector('#submit').addEventListener('click', deletePost)
-    
-   
+document.querySelector('#submit').addEventListener('click', deletePost)
+  
+
+// AQUI É ONDE IRÁ ACONTECER A EDIÇÃO DO POSTAGEM
 const editePost = async (id, post) => {
     await fetch(`http://localhost:3000/Cadastro-Usuarios/${id}`, {
       method: 'PUT',
       headers: {
-        'Accept': 'application/json, text/plain, */*',
+        'Accept': 'application/json, text/plain, /',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(post)
     });
-  }
-
-
-
-
-
+}
 
